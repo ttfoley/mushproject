692606bdcd67c77cb43233c5dbd71acdde36d34d
Thomas <ttfoley@gmail.com>
2024-12-15 10:46:18 -0800

    small change for better print statements

--- control/led_control/app/main.py
+++ control/led_control/app/main.py
@@ -20,9 +20,10 @@ state_output_defs = {"Off":[output_value("pin25","Off"),output_value("pin33","Of
                      "Unknown":[output_value("pin25","Unknown"),output_value("pin33","Unknown")]}
 
 #for now only one rule per state
-state_rules = {"Off":transition_rule("0","Off","HumidOn",10*60),"HumidOn":transition_rule("1","HumidOn","Humidify",15),
-               "Humidify":transition_rule("2","Humidify","FanOff",1*60),"FanOff":transition_rule("3","FanOff","Off",15),
+state_rules = {"Off":transition_rule("0","Off","HumidOn",15*60),"HumidOn":transition_rule("1","HumidOn","Humidify",15),
+               "Humidify":transition_rule("2","Humidify","FanOff",1.5*60),"FanOff":transition_rule("3","FanOff","Off",15),
                "Unknown":transition_rule("4","Unknown","Off",0)}
+
 def main():
     #TODO: verify the connection, add reconnect logic
     mqtt_handler.loop_start()
@@ -32,7 +33,8 @@ def main():
     #Give it some time to fetch values
     humid_control.update_state()
     print(humid_control.current_state.name,humid_control.desired_state.name,humid_control.time_in_state)
-    
+    last_update_time = datetime.now()
+    update_frequency = 60
     while True:
         #print(humid_control.current_state.name,humid_control.desired_state.name,humid_control.time_in_state)
         humid_control.update_state()
@@ -41,11 +43,17 @@ def main():
             print(f"Changed desired state to {humid_control.desired_state.name} at {datetime.now()}.  Current state is: {humid_control.current_state.name}") 
         if humid_control.current_state.name != humid_control.previous_state.name:
             print(f"Verified state change to {humid_control.current_state.name} at {datetime.now()} from state: {humid_control.previous_state.name}")
-        ##Changing to only write the desired state if it has changed
-        if humid_control.current_state.name != humid_control.desired_state.name:
+        ##Changing to only write the desired state if it's not there.
+        ## I don't know if this is sufficient, who knows exactly how MQTT message que stuff works, but 
+        ## as long it's not in desired state, it will keep trying.
+        if not humid_control.in_desired_state():
             humid_control.write_desired_state()
-        
+            #Don't want to spam
+            time.sleep(1)
 
+        if (datetime.now()-last_update_time).total_seconds() > update_frequency:
+            print(humid_control.current_state.name,humid_control.desired_state.name,humid_control.time_in_state)
+            last_update_time = datetime.now()
 
     mqtt_handler.loop_stop()
 
