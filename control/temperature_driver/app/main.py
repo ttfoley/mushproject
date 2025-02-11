8f86c5e260458e3a7e3cab6d34e4d4bb060bbc29
Thomas <ttfoley@gmail.com>
2025-02-10 19:38:12 -0800

    updated paths

--- control/temperature_driver/app/main.py
+++ control/temperature_driver/app/main.py
@@ -1,8 +1,14 @@
 import os
 import sys
 current_dir = os.path.dirname(os.path.abspath(__file__))
-lib_path = os.path.join(current_dir, '../../common')
-config_path = os.path.join(current_dir, '../config')
+# Use Docker paths if in container, otherwise use relative paths
+if os.getenv('IN_DOCKER'):
+    lib_path = '/app/common'
+    config_path = '/app/config'
+else:
+    lib_path = os.path.join(current_dir, '../../common')
+    config_path = os.path.join(current_dir, '../config')
+
 sys.path.append(lib_path)
 
 from fsm_runner import FSMRunner
