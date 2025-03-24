3664f526ddf853da288ff6773a2979804f65f338
Thomas <ttfoley@gmail.com>
2025-03-24 12:10:42 -0700

    fixed up points messenger, was sometimes not publishing on changed value, something subtle I think caused by have equivalent states (off, locked) in humidity driver

--- control/governor/scratchhackery/fake_pwm_heater.py
+++ control/governor/scratchhackery/fake_pwm_heater.py
@@ -15,8 +15,8 @@ MQTT_CLIENT_ID: str = "fake_pwm_heater"
 COMMAND_TOPIC = "mush/drivers/temperature_driver/command/state"
 
 # PWM settings (in seconds)
-TIME_ON = 20
-TIME_OFF = 5   
+TIME_ON = 0.5
+TIME_OFF = 60  
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
